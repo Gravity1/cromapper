3f9fb93da011292ef0bacd8017c9742e137a14c5
ngahika01 <mykenjoroge01@gmail.com>
2022-02-28 23:04:34 +0300

    Added theme

---
+++ src/config/theme.js
@@ -0,0 +1,55 @@
+import { createTheme } from "@mui/material";
+
+const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#827717",
+    },
+    secondary: {
+      main: "#8BC34A",
+      contrastText: "#ffffff",
+    },
+    divider: "#ffffff",
+    background: {
+      default: "#fffff",
+    },
+    success: {
+      main: "#4BDE97",
+    },
+    warning: {
+      main: "#FFB648",
+    },
+    danger: {
+      main: "#F26464",
+    },
+  },
+});
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#2F49D1",
+    },
+    secondary: {
+      main: "#B6C1E9",
+      contrastText: "#B9B9B9",
+    },
+    divider: "#ffffff",
+    background: {
+      default: "#F0F2FA",
+    },
+    success: {
+      main: "#4BDE97",
+    },
+
+    warning: {
+      main: "#FFB648",
+    },
+    danger: {
+      main: "#F26464",
+    },
+  },
+});
+
+export { lightTheme, darkTheme };
