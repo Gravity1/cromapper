c7517fc3144a11cce099e8ff99e69e72041a040b
gravity7 <raphitto.rn@gmail.com>
2022-03-10 10:47:51 +0300

    Login with google good

--- src/Login.js
+++ src/Login.js
@@ -2,82 +2,87 @@ import React from 'react'
 import './Login.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import ForgotPass from './ForgotPass';
+import GoogleLogin from 'react-google-login'
+
 
 function Login() {
 
-  const signUpDiv=
+  // function responseGoogle(response) {
+  //   console.log(response);
+  //   // console.log(response.profileObj);
+  // }
+
+  const signUpDiv =
     <div style={{ marginTop: "2em" }}>
-    <form
-      onSubmit={handleSubmit}
+      <form
+        onSubmit={handleSubmit}
 
 
-    >
-      <div className="form_div">
-        <div><h1>Login</h1></div>
-        <div style={{ textAlign: "center" }}><p>Sign in to continue</p></div>
-        <div style={{ marginBottom: "2em" }, { margintTop: "2em" }}>
-          <label htmlFor="email">PERSONAL/WORK EMAIL</label><br />
-          <input type="email" id="email" name="email" />
-        </div>
+      >
+        <div className="form_div">
+          <div><h1>Login</h1></div>
+          <div style={{ textAlign: "center" }}><p>Sign in to continue</p></div>
+          <div style={{ marginBottom: "2em" }, { margintTop: "2em" }}>
+            <label htmlFor="email">PERSONAL/WORK EMAIL</label><br />
+            <input type="email" id="email" name="email" />
+          </div>
 
-        <div style={{ marginBottom: "2em" }}>
-          <label htmlFor="password">PASSWORD</label><br />
-          <input type="password" id="password" name="password" />
-        </div>
+          <div style={{ marginBottom: "2em" }}>
+            <label htmlFor="password">PASSWORD</label><br />
+            <input type="password" id="password" name="password" />
+          </div>
 
-        <div className=""
-          style=
-          {
-            { marginBottom: "1em" }
-          }>
-
-          <button
-
-            style={
-              { borderRadius: "0.5em" }
-            }
-            type="submit"
-            value="submit"
-            className="btn btn-light login_button">
-            log in
-          </button>
-
-        </div>
-        <div style={{ marginBottom: '1em' }}>
-          <span id="or">
-            OR
-          </span>
-        </div>
-
-        <div
-          style={
-            { width: "100%" }
-          }
-        >
-          <button
-            className="login-with-google-btn"
-            type=""
+          <div className=""
             style=
             {
-              { width: 'inherit' }
+              { marginBottom: "1em" }
             }>
-            Continue with Google
-          </button>
-        </div>
 
-        <br />
-        <div>
-          <p>
-            {/* <Link>Forgot password ?</Link> */}
-            Forgot password ?
-          </p>
-        </div>
-      </div>
-    </form>
-  </div>
+            <button
 
-  const fPassDiv=<ForgotPass/>;
-  
+              style={
+                { borderRadius: "0.5em" }
+              }
+              type="submit"
+              value="submit"
+              className="btn btn-light login_button">
+              log in
+            </button>
+
+          </div>
+          <div style={{ marginBottom: '1em' }}>
+            <span id="or">
+              OR
+            </span>
+          </div>
+
+          <div
+            style={
+              { width: "100%" }
+            }
+          >
+            <GoogleLogin
+              clientId='715177264735-fq5okaiiv0r7jde23son3o5p83jb6ah0.apps.googleusercontent.com'
+              buttonText='Login'
+              onSuccess={(response)=>{console.log(response)}}
+              onFailure={(response)=>{console.log(response)}}
+              cookiePolicy={'single_host_origin'}
+            />
+          </div>
+
+          <br />
+          <div>
+            <p>
+              {/* <Link>Forgot password ?</Link> */}
+              Forgot password ?
+            </p>
+          </div>
+        </div>
+      </form>
+    </div>
+
+  const fPassDiv = <ForgotPass />;
+
 
   function handleSubmit(e) {
     e.preventDefault(); console.log('You clicked submit.');
@@ -99,9 +104,9 @@ function Login() {
           </nav>
 
         </div>
-       
-      {signUpDiv}
-      {/* {fPassDiv} */}
+
+        {signUpDiv}
+        {/* {fPassDiv} */}
       </div>
     </div>)
 
