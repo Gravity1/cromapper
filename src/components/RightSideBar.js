4f614d4d0b8fffcecede9c4ac4ac0f75075d49f5
gravity7 <raphitto.rn@gmail.com>
2022-03-25 12:15:48 +0300

    added redux module, code hygiene

--- src/components/RightSideBar.js
+++ src/components/RightSideBar.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useTheme } from "@emotion/react";
-import ReactWeather, { useOpenWeather } from "react-open-weather";
 import { useNavigate } from "react-router-dom";
 
 import {
