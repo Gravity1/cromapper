7dd2dd811951043d8147d45581adbf82f971291c
ngahika01 <mykenjoroge01@gmail.com>
2022-03-29 15:54:19 +0300

    Added field actions

--- src/store.js
+++ src/store.js
@@ -1,10 +1,21 @@
 import { combineReducers, createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import {
+  fieldCreateReducer,
+  fieldDeleteReducer,
+  fieldsGetReducer,
+  fieldUpdateReducer,
+} from "./reducers/fieldReducers";
 import { fileSaveReducer } from "./reducers/fileReducers";
 
 const reducer = combineReducers({
   fileSave: fileSaveReducer,
+  fieldCreate: fieldCreateReducer,
+  fieldsGet: fieldsGetReducer,
+  fieldGet: fieldsGetReducer,
+  fieldDelete: fieldDeleteReducer,
+  fieldUpdate: fieldUpdateReducer,
 });
 
 const initialState = {};
